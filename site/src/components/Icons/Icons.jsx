a39fa2ec013502296315ca56bc8dcdec15f0d0ab
Pedro Lucas <pedrogmobile2@gmail.com>
2026-08-13 20:11:28 -0300

    feat(contato): adiciona download do currículo em PDF
    Nova linha na lista de contatos com link para curriculo-pedro-lucas.pdf
    (hospedado em site/public), ícone de download e atributo download.
    Atualiza .gitignore e skill git-conventions: o PDF público é versionado
    em site/public; local_files/ segue privado.

--- site/src/components/Icons/Icons.jsx
+++ site/src/components/Icons/Icons.jsx
@@ -37,3 +37,11 @@ export function LinkedinIcon(props) {
     </Icon>
   )
 }
+
+export function DownloadIcon(props) {
+  return (
+    <Icon {...props}>
+      <path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z" />
+    </Icon>
+  )
+}
